85dd406c80d3bfa679b364043dbb60da985c75e5
Nicola <nic00la.kaleta@gmail.com>
2025-12-16 21:26:48 +0100

    Typ Boolean - wartość logiczna

---
+++ basics/01 data/04 type Boolean.py
@@ -0,0 +1,14 @@
+t = True
+f = False
+
+print(type(t)) # <class 'bool'>
+
+print(20 > 6) # 20 większe od 6 ==> True
+print(4 < 2) # 4 mniejsze od 2 ==> False
+
+number = 4
+
+if number > 5:
+    print("Number większe od 5")
+else:
+    print("Number mniejsze od 5")
