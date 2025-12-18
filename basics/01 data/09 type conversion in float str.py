a7f3a3de1c40360c148c5f8c2d7a7d8f356c9d6c
Nicola <nic00la.kaleta@gmail.com>
2025-12-18 06:15:18 +0100

    Konwersje typów - ćwiczenie

---
+++ basics/01 data/09 type conversion in float str.py
@@ -0,0 +1,22 @@
+floatNum = 23.2424235
+intNum = int(floatNum)
+
+print(type(intNum))
+print(intNum)
+
+print(int(" 678     ")) 
+print(int(99))
+
+intNum = 56
+float1 = float(intNum)
+print(type(float1))
+print(float1)
+
+str1 = "123.554723578"
+float2 = float(str1)
+print(type(float2))
+print(float2)
+print(float(89.789))
+
+print(f"Wartość float1: {str(float1)} {str(78)} {str([1, 2, 3, 4])}")
+print("Wartość float1", float1, " ", 78, [1, 2, 3])
