4840a98d071750f7535f8d5e9543f279357b52e9
Nicola <nic00la.kaleta@gmail.com>
2025-12-20 05:34:27 +0100

    parzyste liczby - zadanie

---
+++ challenges/03 if and loops/11. even numbers.py
@@ -0,0 +1,14 @@
+# 1) Stwórz krotkę z wartościami od 1 do 10
+# 2) Zrób pętle for in z krotką i wyświetl w konsoli
+#    tylko parzyste liczby. Skorzystaj z instrukcji
+#    warunkowej if oraz operatora % zwracającego resztę
+#    z dzielenia. Jeśli reszta z dzielenia przez 2 
+#    będzie równa 0 to nie ma reszty, tym samym liczba 
+#    jest parzysta
+
+liczby = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+for num in liczby:
+    parzysta = num % 2 == 0
+    if parzysta:
+        print(f"Liczba {num} jest parzysta")
