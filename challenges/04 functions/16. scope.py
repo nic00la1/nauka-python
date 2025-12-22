feffb03e94d6a6012a7e5fc813574d697ceb5fe9
Nicola <nic00la.kaleta@gmail.com>
2025-12-22 21:21:40 +0100

    Scope zasięg zmiennych - ćwiczenia

---
+++ challenges/04 functions/16. scope.py
@@ -0,0 +1,10 @@
+# Pracownicy w liście z zwiększoną pensją
+# 1) Stwórz globalną zmienną employees, która jest pustą listą
+# 2) Napisz funkcję addEmployee, która przyjmuje email i salary, wewnątrz stwórz
+#    słownik z tymi samymi parametrami. Następnie dodaj go do globalnej listy
+#    employees stosując funkcję append, np. someList.apend(newElement)
+# 3) Wywołaj funkcję addEmployee dla trzech dowolnych pracowników
+#    o pensjach 6000, 8000 i 10000, wpisz dowolne maile
+# 4) Dodaj funkcję increaseSalary z dwoma argumentami: employees i pctIncrease
+#    Jako pierwszy argument będzie przekazywana lista pracowników, a do drugiego
+#    war
