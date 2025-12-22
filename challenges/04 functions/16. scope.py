ad0a5d59732614dd478436acec9780646c7dc1f6
Nicola <nic00la.kaleta@gmail.com>
2025-12-22 21:57:34 +0100

    Scope - zadanie (pracownicy z zwiększoną pensją)

--- challenges/04 functions/16. scope.py
+++ challenges/04 functions/16. scope.py
@@ -2,9 +2,45 @@
 # 1) Stwórz globalną zmienną employees, która jest pustą listą
 # 2) Napisz funkcję addEmployee, która przyjmuje email i salary, wewnątrz stwórz
 #    słownik z tymi samymi parametrami. Następnie dodaj go do globalnej listy
-#    employees stosując funkcję append, np. someList.apend(newElement)
+#    employees stosując funkcję append, np. someList.append(newElement)
 # 3) Wywołaj funkcję addEmployee dla trzech dowolnych pracowników
 #    o pensjach 6000, 8000 i 10000, wpisz dowolne maile
 # 4) Dodaj funkcję increaseSalary z dwoma argumentami: employees i pctIncrease
 #    Jako pierwszy argument będzie przekazywana lista pracowników, a do drugiego
-#    war
+#    wartość podwyżki, np. 15. Przejdź po wszystkich pracownikach i zwiększ
+#    pensję pracowników o przekazaną wartość procentową pctIncrease
+# 5) Zwiększ pensję pracowników z funkcją increaseSalary o 20%, wyświetl
+#    listę w terminalu.
+#
+
+employees = []
+
+def displayEmployees(employees):
+    for employee in employees:
+        print(employee)
+
+def addEmployee(email, salary): 
+    employee = {
+        "email": email,
+        "salary": salary
+    }
+
+    employees.append(employee)
+6
+addEmployee("Ola@example.com", 6000)
+addEmployee("Weronika@exmaple.com", 8000)
+addEmployee("Amelia@exmaple.com", 10000)
+print("Wszyscy pracownicy:", len(employees))
+displayEmployees(employees)
+
+
+def increaseSalary(employees, pctIncrease):
+    for employee in employees:
+        employee["salary"] = employee["salary"] + (employee["salary"] * (pctIncrease / 100))
+
+pctIncrease = 20
+increaseSalary(employees, 20)
+
+print("\nLista pracówników po podwyżce", pctIncrease, "%:")
+displayEmployees(employees)
+
