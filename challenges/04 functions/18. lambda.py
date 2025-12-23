c0f98ba95dc692c3c5460aacc47f6f9981dfac84
Nicola <nic00la.kaleta@gmail.com>
2025-12-23 06:05:09 +0100

    Lambda - zadanie

---
+++ challenges/04 functions/18. lambda.py
@@ -0,0 +1,15 @@
+# Zadanie z lambdą i map
+# 1) Stwórz listę names z wartościami Ola, Ania, Kasia
+# 2) Z pomocą mapy i lambdy, dodaj do każdego imienia tekst " Kowalska"
+# 3) Wyświetl nową listę w konsoli
+# 4) Przefiltruj nową listę ze względu na długość tekstu, zachowaj
+#    w nowej liście tylko te, które mają więcej niż 12 znaków
+#    Pokaż przefiltrowaną listę w konsoli.
+
+names = ("Ola", "Ania", "Kasia")
+
+result = list(map(lambda name: name + " Kowalska", names))
+print(result)
+
+result2 = list(filter(lambda name: len(name) > 12, result))
+print(result2)
