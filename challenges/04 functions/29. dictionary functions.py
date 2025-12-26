cd7c0958f2943e0b2b2743edd9a768e674d9d114
Nicola <nic00la.kaleta@gmail.com>
2025-12-26 13:36:26 +0100

    operacja na slownikach

---
+++ challenges/04 functions/29. dictionary functions.py
@@ -0,0 +1,6 @@
+# Zadanie - zarządzanie książką adresową
+# W tym zadaniu będziesz używać słowników do zarządzania prostą
+# książką adresową. Nauczysz się dodawać, usuwać, kopiować oraz
+# przeszukiwać dane w słowniku.
+#
+# 1) Stwórz słownik `addressBook` zawi
