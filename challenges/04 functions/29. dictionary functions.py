6c3719ba886cb5c040bb385935326361468b9535
Nicola <nic00la.kaleta@gmail.com>
2025-12-26 14:08:33 +0100

    zarządzanie książką adresową

--- challenges/04 functions/29. dictionary functions.py
+++ challenges/04 functions/29. dictionary functions.py
@@ -3,4 +3,49 @@
 # książką adresową. Nauczysz się dodawać, usuwać, kopiować oraz
 # przeszukiwać dane w słowniku.
 #
-# 1) Stwórz słownik `addressBook` zawi
+# 1) Stwórz słownik `addressBook` zawierający początkowo jedną
+#    osobę: Jan Kowalski, mieszka w Gdańsku, kod pocztowy 80-000.
+# 2) Dodaj do książki adresowej nową osobę: Mirosława Nowak, mieszka w 
+#    Warszawie, kod pocztowy 00-001.
+# 3) Usuń Jan Kowalski z książki adresowej.
+# 4) Skopiuj książkę adresową do nowej zmiennej i sprawdź, czy 
+#    kopia jest identyczna (porównaj referencje i zawartość).
+# 5) Sprawdź, czy w skopiowanej książce adresowej jest osoba
+#    mieszka w Krakowie. Jeśli nie, wyświetl odpowiedni komunikat.
+# 6) Wyświetl wszystkie klucze i wartości w książce adresowej.
+
+addressBook = {
+    "Jan Kowalski" : {
+        "city": "Gdańsk",
+        "postalCode": "80-000"
+    }
+}
+
+print("Początkowa wartość książki adresowej:", addressBook)
+
+addressBook["Mirosława Nowak"] = {
+    "city" : "Warszawa",
+    "postalCode": "00-001"
+}
+
+print("Książka adresowa po aktualizacji:", addressBook)
+
+del addressBook["Jan Kowalski"]
+print("Książka adresowa po aktualizacji:", addressBook)
+
+addressBookCopy = addressBook.copy()
+
+print("Czy referencje są identyczne:", addressBook is addressBookCopy)
+print("Czy zawartość jest identyczna:", addressBook == addressBookCopy)
+
+found = False
+for person in addressBook.values():
+    if person["city"] == "Kraków":
+        found = True
+        print("W książce adresowej jest osoba z Krakowa")
+
+if not found:
+    print("W książce adresowej nie ma osoby z Krakowa")
+
+print("Klucze:", addressBook.keys())
+print("Wartości:", addressBook.values())
