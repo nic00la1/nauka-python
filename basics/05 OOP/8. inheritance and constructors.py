cf4c9da2d1a927dc892fac934dd917c856874aa8
Nicola <nic00la.kaleta@gmail.com>
2025-12-28 16:03:29 +0100

    Dziedziczenie a konstruktory

---
+++ basics/05 OOP/8. inheritance and constructors.py
@@ -0,0 +1,48 @@
+class Person: 
+    def __init__(self, name, surname, city):
+        self.name = name
+        self.surname = surname
+        self.city = city
+        print("Person constructor")
+
+    def printPersonData(self):
+        print("Person.printPersonData:", self.name,
+              self.surname, self.city)
+
+person1 = Person("Ola", "Kowalska", "Kraków")
+person1.printPersonData()
+
+class Employee(Person):
+    def __init__(self, name, surname, city, companyName, salary):
+        Person.__init__(self, name, surname, city)
+        self.companyName = companyName
+        self.salary = salary
+
+        print("Employee constructor!")
+
+    def printEmployeeData(self):
+        print("Employee.printEmployeeData:", self.name, self.surname, self.companyName, self.salary)
+
+print()
+employee1 = Employee("Kasia", "Kot", "Lublin", "Tech Ltd", 10000)
+employee1.printPersonData()
+employee1.printEmployeeData()
+
+class Manager(Employee):
+    def __init__(self, name, surname, city, companyName, salary, department):
+        Employee.__init__(self, name, surname, city, companyName, salary)
+        self.department = department
+        print("Manager constructor!")
+
+    def hireEmployee(self):
+        print("Hire employee")
+
+    def printManagerData(self):
+        print("Manager data:", self.name, self.surname, self.department)
+
+print()
+manager1 = Manager("Teresa", "Ważka", "Wrocław", "Tech2 Ltd", 15000, "IT")
+manager1.printPersonData()
+manager1.printEmployeeData()
+manager1.hireEmployee()
+manager1.printManagerData()
