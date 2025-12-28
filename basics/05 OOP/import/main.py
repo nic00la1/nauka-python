b1b8c64706701be46cc965947d1f05a2da8118c3
Nicola <nic00la.kaleta@gmail.com>
2025-12-28 16:42:33 +0100

    moduły instrukcja import oraz from import

---
+++ basics/05 OOP/import/main.py
@@ -0,0 +1,10 @@
+import user
+from manager import Manager
+
+print("main:", user.user1)
+
+employee1 = user.Employee("Eryk")
+print("main:", employee1)
+
+manager1 = Manager("Waldek")
+print("main:", manager1)
