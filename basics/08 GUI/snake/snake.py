726499fac430efb1888a3a2ba940ac1abc3014c9
Nicola <nic00la.kaleta@gmail.com>
2026-01-03 09:07:59 +0100

    Turtle - Gra Snake - implementacja klasy Food

--- basics/08 GUI/snake/snake.py
+++ basics/08 GUI/snake/snake.py
@@ -11,3 +11,13 @@ class Snake:
         print("Snake reset")
 
         self.segments = []
+        self.addSegment(self.startX, self.startY)
+
+    def addSegment(self, x, y):
+        t = turtle.Turtle("square")
+        t.hideturtle()
+        t.penup()
+        t.goto(x, y)
+        t.color("red")
+        t.showturtle()
+        self.segments.append(t)
