25acc6fd6b0f515f5c72b9b00119f36b54bdb2fd
Nicola <nic00la.kaleta@gmail.com>
2026-01-04 12:37:29 +0100

    Tkinter - Dane finansowe spółek giełdowych z serwera cz.2 yFinanse

--- basics/08 GUI/tkinter stock info.py
+++ basics/08 GUI/tkinter stock info.py
@@ -12,7 +12,7 @@ entry.pack(side=tk.RIGHT)
 topWidget.pack()
 
 scrollbar = tk.Scrollbar(window)
-textBox = tk.Text(window, height=10, width=70, padx=5, pady=5,
+textBox = tk.Text(window, height=15, width=90, padx=5, pady=5,
             font=("Helvetica", 12))
 
 scrollbar.pack(side=tk.RIGHT, fill = tk.Y)
@@ -21,6 +21,7 @@ scrollbar.config(command = textBox.yview)
 textBox.config(yscrollcommand = scrollbar.set)
 
 def downloadData(e):
+    textBox.delete("1.0", tk.END)
     stock = str(e.widget.get())
 
     if not stock:
@@ -32,6 +33,19 @@ def downloadData(e):
 
     stockData = yf.Ticker(stock)
     print(stockData.info)
+
+    textBox.insert(tk.END, "Ticker: " + stock + " \n\n")
+    for key in stockData.info.keys():
+        try:
+            v = str(key) + ": " + stockData.info[str(key)] + " \n\n "
+            textBox.insert(tk.END, v)
+        except:
+            pass
+
+    # 1mo, 1m, 1d, 1y, 1wk
+    history = stockData.history(period="1mo", interval="1d")
+    textBox.insert(tk.END, history)
+
 
 entry.bind("<Return>", downloadData)
 
